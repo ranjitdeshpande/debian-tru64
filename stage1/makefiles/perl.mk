85e582dcec45f957367409284bed698cbe0ad47d
Ranjit Deshpande <ranjitdeshpande@hotmail.com>
2018-03-06 07:19:15 -0800

    Add perl 5.26.1

---
+++ stage1/makefiles/perl.mk
@@ -0,0 +1,24 @@
+# Copyright 2018 Ranjit Deshpande <ranjitdeshpande@hotmail.com>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+NAME		:= perl
+VERSION		:= 5.26.1
+CONFIGURE	= ./configure.gnu
+
+CC		= gcc
+export CC
+
+include makefiles/pkgbuild.mk
